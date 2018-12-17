3adf0076806b82f45892fc16aaa77286ed34014c
Zachary Justice <zach.justice@slalom.com>
2018-12-17 09:05:38 -0500

    stuff

---
+++ startup.sh
@@ -0,0 +1,13 @@
+touch creds.json
+touch cypress.json
+echo '
+{ 
+    "env": { 
+        "access_code": "",
+        "first_name": "",
+        "last_name": "",
+        "email": "george@aol.net",
+        "company": "XPO"
+    }
+}
+' > cypress.json
